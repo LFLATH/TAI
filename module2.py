e3ccdb55ec92bf8393ec6143d57daf0589b587bb
Alexander Sweet <sweetxander@gmail.com>
2022-03-23 20:56:55 -0400

    Revert "$$"
    This reverts commit 02c4b91575d3f4058970184c3a16f808696967c5.

--- module2.py
+++ module2.py
@@ -1,17 +1,7 @@
 from PIL import Image
-import numpy
-import pandas
-
-numpy.set_printoptions(threshold=numpy.inf)
+from numpy import array
 
 img = Image.open(r"C:/users/Sweet/source/repos/assets/test1.jpg")
 
-simg = img.resize((64,64),resample=Image.NEAREST)
-m = array(simg)
-simg.show()
-
-simg.convert("P", palette=Image.ADAPTIVE, colors=8) # just temporary, later we can convert array values to hexidecimal to get a single channel 4 bit per pixel image
-
-simg.show
-
-
+ar = array(img)
+ar
