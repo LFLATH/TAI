02c4b91575d3f4058970184c3a16f808696967c5
Alexander Sweet <sweetxander@gmail.com>
2022-03-23 20:46:47 -0400

    $$  0

--- module2.py
+++ module2.py
@@ -1,7 +1,17 @@
 from PIL import Image
-from numpy import array
+import numpy
+import pandas
+
+numpy.set_printoptions(threshold=numpy.inf)
 
 img = Image.open(r"C:/users/Sweet/source/repos/assets/test1.jpg")
 
-ar = array(img)
-ar
+simg = img.resize((64,64),resample=Image.NEAREST)
+m = array(simg)
+simg.show()
+
+simg.convert("P", palette=Image.ADAPTIVE, colors=8) # just temporary, later we can convert array values to hexidecimal to get a single channel 4 bit per pixel image
+
+simg.show
+
+
