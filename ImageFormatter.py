11960689229b57ec3ba9767df1246c4ede53cf85
Ludwig Lechtreck <ludwiglechtreck@gmail.com>
2022-03-24 21:34:43 -0400

    Made the binary converter create an array of rgb values. Isn't finished yet, but works in conjunctio with formatter. Also formatter now returns the formatted image like a good method.

--- ImageFormatter.py
+++ ImageFormatter.py
@@ -6,8 +6,6 @@ import pandas
 numpy.set_printoptions(threshold=numpy.inf)#Sets the length at which an array is truncated, while printing, to infinity
 def format_Image(img):#Turned the file into a method
     simg = img.resize((64,64),resample=Image.NEAREST)#Resize the image to 64x64 pixels using the Nearest Function from PIL
-    m = array(simg)#Define an array from the image
-    simg.show()#Show the image
-
     simg.convert("P", palette=Image.ADAPTIVE, colors=8) # just temporary, later we can convert array values to hexidecimal to get a single channel 4 bit per pixel image
-    simg.show
+    return simg
+    
