506a0f3e7a1dd05e9e431c15798e8d8174deefde
Alexander Sweet <sweetxander@gmail.com>
2022-03-26 19:16:50 -0400

    no comment

---
+++ Network.py
@@ -0,0 +1,12 @@
+import numpy
+from HSVConverter import huearray
+from Neuron import Neuron 
+
+input = huearray
+
+huearray
+
+for n in range(len(input)):
+    for p in range(len(input[0])):
+        feedforward()#youre gonna ahve to exlpain how to use classes to me agian
+        input = input + 1 #idk how to add one to the bracketed value
