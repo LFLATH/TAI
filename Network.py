5d0400780ad9a6f82a33891c35aa384ce0824975
sweet00000 <88102370+sweet00000@users.noreply.github.com>
2022-03-26 23:40:11 -0400

    Update Network.py

--- Network.py
+++ Network.py
@@ -7,19 +7,3 @@ class Network:
   def __init__(self):
     weights = numpy.random.rand(28, 28)
     bias = 0
-
-    self.h1 = Neuron(weights, bias)
-    self.h2 = Neuron(weights, bias)
-    self.o1 = Neuron(weights, bias)
-
-  def feedforward(self, x):
-    out_h1 = self.h1.feedforward(x)
-    out_h2 = self.h2.feedforward(x)
-
-    # The inputs for o1 are the outputs from h1 and h2
-    out_o1 = self.o1.feedforward(numpy.array([out_h1, out_h2]))
-
-    return out_o1
-
-network = Network()
-network.feedforward(huearray)
