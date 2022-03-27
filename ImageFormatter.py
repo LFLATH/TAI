914b9e35f1879c7eecfa2f36c89cbeb629a92625
sweet00000 <88102370+sweet00000@users.noreply.github.com>
2022-03-27 11:00:08 -0400

    Update ImageFormatter.py

--- ImageFormatter.py
+++ ImageFormatter.py
@@ -7,5 +7,4 @@ numpy.set_printoptions(threshold=numpy.inf)#Sets the length at which an array is
 
 def format_Image(img):#Turned the file into a method
     simg = img.resize((28,28),resample=Image.NEAREST)#Resize the image to 28x28 pixels using the Nearest Function from PIL
-    return simg
-    
+    return simg  
