08a4710e84ffc1d5e7d267310f9766145e446185
Alexander Sweet <sweetxander@gmail.com>
2022-03-27 10:09:46 -0400

    layer1 of network

--- Network.py
+++
@@ -1,9 +0,0 @@
-import numpy
-import Neuron
-from HSVConverter import huearray
-    
-class Network:
-
-  def __init__(self):
-    weights = numpy.random.rand(28, 28)
-    bias = 0
--- Neuron.py
+++ Neuron.py
@@ -16,3 +16,14 @@ def sigmoid(t):
     #This function will compute the final value of our NN
     return 1 / (1 + numpy.exp(-t))
 
+from HSVConverter import huearray
+
+x = huearray
+
+weights = numpy.random.rand(28,28)-.5 #sets a random weight between -.5 and .5 for each neuron/pixel in the first layer
+biases = numpy.zeros((28,28))#creates biases for each of the 784 neurons
+
+layer1 = Neuron(weights, biases)
+
+layer1.feedforward(x)
+
