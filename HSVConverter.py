b3c38e29fc73b4a6b0ae56694cee9f9bcc147f8a
Ludwig Lechtreck <ludwiglechtreck@gmail.com>
2022-03-27 21:45:58 -0400

    Making a math file

--- HSVConverter.py
+++ HSVConverter.py
@@ -1,44 +1,38 @@
 from ImageFormatter import *
 from numpy import asarray
 from matplotlib import pyplot as plt
-img = Image.open('Test_Images/t1.jpg') #Loads the image
-#img = format_Image(img)#Formats the Image
-img = format_Image(img)
-data = asarray(img)#Makes image into an array of rgb values
-#print(data[0][0])#Prints the data
-#img.show()
-huearray = numpy.zeros((len(data) * len(data[0])))
-step = 0
-for i in range(len(data)):
-    for j in range(len(data[0])):
-        
-        red = data[i][j][0]
-        green = data[i][j][1]
-        blue = data[i][j][2]
-        red = red / 255
-        green = green / 255
-        blue = blue / 255
-        Cmax = max(red, green, blue)
-        Cmin = min(red, green, blue)
-        delta = Cmax - Cmin
-        if Cmax == Cmin:
-            h = 0
-        elif Cmax == red:
-            h = (60*(((green - blue)/delta)%6))
-        elif Cmax == green:
-            h = (60*(((blue -red)/delta)+2))
-        elif Cmax == blue:
-            h = (60*(((red-green)/delta)+4))
-        elif Cmax == 0:
-            s = 0
-        else:
-            s = (delta / Cmax)
-        v = Cmax
-        huearray[step] = h
-        step = step + 1
-#bins1 = [None] * 360
-#for v in range(0, 360):
-#    bins1[v] = v
-#plt.hist(huearray,  bins = bins1)
-#plt.title("Proving Python is better")
-#plt.show()
+def makeArray(img): #We can plug in an image and convert that to a huearray
+    img = format_Image(img)#img = format_Image(img)#Formats the Image
+    data = asarray(img)#Makes image into an array of rgb values
+    #print(data[0][0])#Prints the data
+    #img.show()
+    huearray = numpy.zeros((len(data) * len(data[0])))
+    step = 0
+    for i in range(len(data)):
+        for j in range(len(data[0])):
+            
+            red = data[i][j][0]
+            green = data[i][j][1]
+            blue = data[i][j][2]
+            red = red / 255
+            green = green / 255
+            blue = blue / 255
+            Cmax = max(red, green, blue)
+            Cmin = min(red, green, blue)
+            delta = Cmax - Cmin
+            if Cmax == Cmin:
+                h = 0
+            elif Cmax == red:
+                h = (60*(((green - blue)/delta)%6))
+            elif Cmax == green:
+                h = (60*(((blue -red)/delta)+2))
+            elif Cmax == blue:
+                h = (60*(((red-green)/delta)+4))
+            elif Cmax == 0:
+                s = 0
+            else:
+                s = (delta / Cmax)
+            v = Cmax
+            huearray[step] = h
+            step = step + 1
+    return huearray
--- Network.py
+++ Network.py
@@ -1,11 +1,28 @@
 import numpy
-from HSVConverter import huearray
+from HSVConverter import makeArray
 import Neuron
+from PIL import Image
 
-x = huearray
-weights = numpy.random.rand(28,28)-.5 #sets a random weight between -.5 and .5 for each neuron/pixel in the first layer
-biases = numpy.zeros((28,28))#creates biases for each of the 784 neurons
 
-layer1 = Neuron(weights, biases)
 
-layer1.feedforward(x)
+img = Image.open('Test_Images/t1.jpg') #Loads the image
+x = makeArray(img)
+
+
+class Network:
+
+    def __init__(self):
+        weights = []
+        biases = []
+        for i in range(0,23521):# Creates the 23,520 weights we need and puts them into a list
+            weights[self.w.format(i)] = numpy.random.normal()
+        for j in range(0, 33):#Creates 32 biases. This is because we have 30 neurons in the hidden layer, and 2 output neurons
+            biases[self.b.format(j)] = numpy.random.normal()
+
+
+    weights = numpy.random.rand(28,28)-.5 #sets a random weight between -.5 and .5 for each neuron/pixel in the first layer
+    biases = numpy.zeros((28,28))#creates biases for each of the 784 neurons
+
+    layer1 = Neuron(weights, biases)
+
+    layer1.feedforward(x)
