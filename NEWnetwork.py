e01def0934ca56d29514848e7c6f8c45eefc9071
Alexander Sweet <sweetxander@gmail.com>
2022-04-11 19:10:57 -0400

    modular net fixes

--- NEWnetwork.py
+++ NEWnetwork.py
@@ -5,6 +5,24 @@ import os
 
 data = np.genfromtxt("data/1722.csv", delimiter=',', skip_header=1)
 
+def sigmoid(x):
+    return 1 / (1 + np.exp(-x))
+
+def dsigmoid(x):
+    s = sigmoid(x)
+    return s * (1 - s)
+
+def relu(x):
+    return np.maximum(0, x)
+def drelu(x):
+    return (x > 0) * 1
+
+def mse(true, pred):
+    return ((true - pred) ** 2).mean()
+def dmse(true, pred):
+    return 2*(true - pred) / np.size(true)
+
+
 class layer:
     def __init__(self):
         self.input = None
@@ -23,6 +41,7 @@ class connected(layer):
     def forward(self, input):
         self.input= input
         self.output = np.dot(input, self.weights) + self.biases
+        return self.output
 
     def backward(self, yerror, rate):
         weights_error = np.dot(yerror, self.input.T)
@@ -32,35 +51,47 @@ class connected(layer):
 
 class activation(layer):
 
-    def __init__(self, activation, dactivation):
-        self.act = activation
-        self.dact = dactivation
-
+    def __init__(self):
+        pass
     def forward(self, input):
         self.input = input
-        self.output = self.act(self.input)
+        self.output = sigmoid(self.input)
         return self.output
 
     def backward(self, yerror, rate):
         #return self.dact(self.input) * yerror
-        return np.multiply(yerror, self.dactivation(self.input))
+        return np.multiply(yerror, dsigmoid(self.input))
 
 
 
-def sigmoid(x):
-    return 1 / (1 + np.exp(-x))
+input = np.reshape([[0, 0], [0, 1], [1, 0], [1, 1]], (4, 1, 2))
+true = np.reshape([[0], [1], [1], [0]], (4, 1, 1))
 
-def dsigmoid(x):
-    s = sigmoid(x)
-    return s * (1 - s)
 
-def relu(x):
-    return np.maximum(0, x)
-def drelu(x):
-    return (x > 0) * 1
+network = [
+    connected(2, 3),
+    activation(),
 
-def mse(true, pred):
-    return ((true - pred) ** 2).mean()
-def dmse(true, pred):
-    return 2*(true - pred) / np.size(true)
+    connected(3, 1),
+    activation()
+]
 
+epochs = 100
+rate = .1
+
+for i in range(epochs):
+    error = 0
+    for x, y in zip(input, true):
+        input = x
+        for layer in network:
+            
+            output = layer.forward(output)
+
+            error += mse(true, output)
+
+            gd = dmse(true, output)
+            for layer in reversed(network):
+                gd = layer.backward(gd, rate)
+
+    error /= len(input)
+    print('%d/%d, error=%d' % (i + 1, epochs, error))
