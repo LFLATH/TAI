c857a8098fd37543af79b0ad4e1a641cfa6e5f67
Ludwig Lechtreck <ludwiglechtreck@gmail.com>
2022-04-21 09:34:55 -0400

    Changing Network

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ for images in os.listdir(img_folder):#Loops through these images
 
 results= numpy.array([1, 1])
 '''
-img1 = Image.open('Test_Images/t1.jpg') #Loads the image
+img1 = Image.open('Test_Images/unripe.jpg') #Loads the image
 img1 = convertHue(img1)
 img2 = Image.open('Test_Images/t2.jpg') #Loads the image
 img2 = convertHue(img2)
@@ -22,24 +22,17 @@ imgarray = numpy.array([img1, img2])
 print(np.shape(img1))
 print(np.shape(img2))
 
-'''
 
-X = np.array([1, 2, 3, 4, 50, 60, 70])
-Y = np.array([1, 1, 1, 1, 0, 0, 0])
+
+X = imgarray
+Y = np.array([1, 1])
 
 network = [
-    Dense(784, 10), Changed to 784 since 784 hsv values, changed to 10 because 10 nodes in the first layer
+    Dense(784, 10),
     Hypertan(),
-    Dense(10, 1), Changed to w10 because 10 input nodes, output to 1 output node 
+    Dense(10, 1), 
     Hypertan()
 ]
 
 # train
 train(network, mse, mse_p, X, Y, epochs=10000, learning_pace=0.1)
-for x in X:
-    pred_num = predict(network, x)
-    if pred_num < 0.5:
-        print("Greater than 10")
-    else:
-        print("Less than 10")
-'''
