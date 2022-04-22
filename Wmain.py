75bf2648397da15ce02ac58286fcadb9e41f71dc
unknown <sweet@gmail.com>
2022-04-21 22:28:17 -0400

    trying rmse and weather data in wmain

---
+++ Wmain.py
@@ -0,0 +1,45 @@
+import os#Allows us to access pictures and files with Python
+from HSVConverter import convertHue
+from ImageFormatter import *#Import the method from ImageFormatter
+import numpy as np
+from numpy import genfromtxt
+from dense import Dense
+from tan import Hypertan
+from mse import mse, mse_p
+from network import train, predict
+
+X = np.genfromtxt("C:/Users/Sweet/source/repos/Weather/data/1722.csv", int, delimiter=',', skip_header=1)
+Y = np.genfromtxt("C:/Users/Sweet/source/repos/Weather/data/1722.csv", int, delimiter=',', skip_header=2)
+X = X[0:100]
+Y = Y[0:100]
+
+
+network = [
+    Dense(3, 3),
+    Hypertan(),
+    Dense(3, 3),
+    Hypertan(),
+]
+
+
+train(network, rmse, drmse, X, Y, epochs=100000, learning_pace=.1)#slowly decreasing this number seems to decrease mse well, maybe casue it is treating the imgarray as a new mini batch #nvm
+
+
+'''
+epochs=10
+learning_pace=.001
+
+for epoch in range(epochs):
+    error = 0
+    for x, y in zip(X, Y):
+        for x_sub in x:
+            output = predict(network, x_sub)
+            
+            error += mse(y, output)
+            
+            grad = mse_p(y, output)
+            for layer in reversed(network):
+                grad = layer.backwardprop(grad, learning_pace)
+    print(f"{epoch + 1}/{epochs}, error={error}")
+
+'''
--- mse.py
+++ mse.py
@@ -5,3 +5,13 @@ def mse(val_true, val_pred):
 
 def mse_p(val_true, val_pred):
     return 2 * (val_pred - val_true) / np.size(val_true)
+
+def rmse(true, pred):
+    return np.sqrt(np.mean(np.power(true - pred, 2)))
+def drmse(true, pred):
+    return 1/(2 * (true-pred) / np.size(true))
+
+drmse(1,40000)
+
+mse_p(1,4)
+rmse(1,4)
