cdab7648231c7743a774561c2eb3c66a00306148
Ludwig Lechtreck <ludwiglechtreck@gmail.com>
2022-04-25 10:05:48 -0400

    Images

--- main.py
+++ main.py
@@ -18,15 +18,13 @@ img1 = Image.open('Test_Images/21S151-03.jpg') #Loads the image
 img1 = convertHue(img1)
 img2 = Image.open('Test_Images/t2.jpg') #Loads the image
 img2 = convertHue(img2)
+
+
 imgarray = numpy.array([img1, img2])
-print(np.shape(img1))
-print(np.shape(img2))
 for img in imgarray:
     img = img.T
-print(np.shape(imgarray[[0]]))
 X = imgarray
 Y = np.array([0, 1])
-
 network = [
     Dense(784, 10), #Turned 1,10 into 784, 10
     Hypertan(),
@@ -35,4 +33,7 @@ network = [
 ]
 
 # train
-train(network, mse, mse_p, X, Y, epochs=10, learning_pace=.001)#slowly decreasing this number seems to decrease mse well, maybe also casue treating the imgarray as a new mini batch #nvm
+train(network, mse, mse_p, X, Y, epochs=1, learning_pace=.0001)#slowly decreasing this number seems to decrease mse well, maybe also casue treating the imgarray as a new mini batch #nvm
+
+for img in imgarray:
+    predict(network, img)
