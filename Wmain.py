1503dae11a04f9cedfca82d44b5a38126e154656
sweet00000 <sweetxander@gmail.com>
2022-04-28 11:50:29 -0400

    deep layers added

--- Wmain.py
+++ Wmain.py
@@ -12,42 +12,34 @@ Y = np.reshape(true, (150, 1, 1))
 network = [
     Dense(4, 4),
     Hypertan(),
-    Dense(4,1),
+    Dense(4,4),
+    Hypertan(),
+    Dense(4, 4),
+    Hypertan(),
+    Dense(4, 4),
+    Hypertan(),
+    Dense(4, 4),
+    Hypertan(),
+    Dense(4, 1),
     Hypertan(),
 ]
 
-'''
-x = X[4]
-y= Y[4]
 
-l = Dense(4, 4)
+train(network, mse, dmse, X, Y, epochs=10000, learning_pace=.001)
 
-l.forwardfeed(x)
-
-'''
-
-
-train(network, rmse, drmse, X, Y, epochs=100, learning_pace=.0001)#decreasing learning rate gradually decreases error
-
-
+print(predict(network, X[5]))
+print(predict(network, X[105]))
 
 
 
 
 # attempting to decrease learning rate gradually
 '''
-i = .001
-for i in range(10):
-    train(network, mse, dmse, X, Y, epochs=40, learning_pace=i)
-    i = i/10
+i = .01
+for i in range(100):
+    i = i/5
+    for j in range(100):
+        train(network, mse, dmse, X, Y, epochs=100, learning_pace=i)
+        i = i*2
 '''
-
-'''
-
-for layer in network:
-        o = layer.forwardfeed(x)
-        #print(o)
-
-'''
-print(predict(network, X[9]))
 
